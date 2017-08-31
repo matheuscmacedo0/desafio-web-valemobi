7163865dd054185b26fb7b415b4dc72741f2b403
matheuscmacedo0 <matheuscmacedo@hotmail.com>
2017-08-31 14:23:51 -0300

    Ajustando a consulta de mercadoria

--- consultar-mercadoria.php
+++ consultar-mercadoria.php
@@ -1,37 +1,102 @@
-<?php
-	include 'conexao.php';
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <!-- Compatibilidade -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Responsividade -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	$pesquisa = $_POST['pesquisa-mercadoria'];
-	$query = ("SELECT * FROM mercadoria WHERE nome_mercadoria LIKE '%".$pesquisa."%' ");
-	$mysqli = mysqli_query($conexao,$query);
+    <title>NegociaShop - Negociação de Mercadorias</title>
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- Compatibilidade -->
+        <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 
-	$row = 1;
-	// $row = mysqli_num_row($mysqli);
+  <body>
+    <nav>
 
-	if ($row > 0) {
-		while ($linha = mysqli_fetch_array($mysqli)) {
-			$codigoMercadoria = $linha['codigo_mercadoria'];
-			$tipoMercadoria = $linha['tipo_mercadoria'];
-			$nomeMercadoria = $linha['nome_mercadoria'];
-			$quantidadeMercadoria = $linha['quantidade_mercadoria'];
-			$precoMercadoria = $linha['preco_mercadoria'];
-			$tipoNegocio = $linha['tipo_negocio'];
+      <div class="container">
 
-			echo "<strong>Codigo da Mercadoria: </strong>".@$codigoMercadoria;
-			echo "<br/><br/>";
-			echo "<strong>Tipo da Mercadoria: </strong>".@$tipoMercadoria;
-			echo "<br/><br/>";
-			echo "<strong>Nome da Mercadoria: </strong>".@$nomeMercadoria;
-			echo "<br/><br/>";
-			echo "<strong>Quantidade: </strong>".@$quantidadeMercadoria;
-			echo "<br/><br/>";
-			echo "<strong>Preco da Mercadoria: </strong>".@$precoMercadoria;
-			echo "<br/><br/>";
-			echo "<strong>Tipo do Negocio: </strong>".@$tipoNegocio;
-			echo "<br/><br/>";
-		}
-	}else{
-		echo "Nenhum resultado foi encontrado";
-	}
+        <div class="col-md-4">
+          <div id="menu" class="menu-lateral">
+            <div id="logotipo">NegociaShop</div>
+            <a href="index.html">Início</a>
+            <a href="cadastrar-mercadoria.html">Cadastrar Mercadoria</a>
+            <a href="consultar-mercadoria.html">Consultar Mercadoria</a>
+          </div>
+        </div>
 
-?>
+      </div>
+      
+    </nav>
+   
+
+
+   	<section id="tabela-pesquisa">
+   	
+	   	<div class="container">
+
+	   		<div class="row">
+
+	   			<div class="col-md-6">
+
+	   				<header id="cabecalho-consultado">
+				    	Resultados para: <?php echo $_POST['pesquisa-mercadoria']?>
+				    </header>
+	   				
+				   	<?php
+						include 'conexao.php';
+
+						$pesquisa = $_POST['pesquisa-mercadoria'];
+						$query = ("SELECT * FROM mercadoria WHERE nome_mercadoria LIKE '%".$pesquisa."%' ");
+						$mysqli = mysqli_query($conexao,$query);
+
+						
+						// $row = mysqli_num_row($mysqli)
+						$row = mysqli_num_rows($mysqli);
+
+						// Criando cabeçalho da tabela
+						echo '<table class="table table-striped"><thead><tr><th>Codigo</th><th>Tipo</th><th>Nome</th>
+								<th>Quantidade</th><th>Preco</th><th>Negocio</th></tr></thead>';
+
+						// Se houver resultado, exiba-o
+						if ($row > 0) {
+							while ($linha = mysqli_fetch_array($mysqli)) {
+								$codigoMercadoria = $linha['codigo_mercadoria'];
+								$tipoMercadoria = $linha['tipo_mercadoria'];
+								$nomeMercadoria = $linha['nome_mercadoria'];
+								$quantidadeMercadoria = $linha['quantidade_mercadoria'];
+								$precoMercadoria = $linha['preco_mercadoria'];
+								$tipoNegocio = $linha['tipo_negocio'];
+
+								echo "<tbody><tr><td align='center'>".$codigoMercadoria."</td>";
+								echo "<td>".$tipoMercadoria."</td>";
+								echo "<td>".$nomeMercadoria."</td>";
+								echo "<td align='center'>".$quantidadeMercadoria."</td>";
+								echo "<td>".$precoMercadoria."</td>";
+								echo "<td>".$tipoNegocio."</td></tr>";
+							}
+						}else{
+							echo "Nenhum resultado foi encontrado";
+						}
+						echo "</tbody</table>";
+
+						mysqli_free_result($mysqli);
+						mysqli_close($conexao);
+
+					?>
+				</div>
+			</div>
+	  	</div>
+   </section>
+
+    <!--Bootstrap-->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
