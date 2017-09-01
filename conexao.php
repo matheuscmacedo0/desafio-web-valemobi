1df297dd73bdc83e36100a4f8856f54f9377aa2c
matheuscmacedo0 <matheuscmacedo@hotmail.com>
2017-09-01 16:18:33 -0300

    Modificando para conexão externa

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-$servidor = 'localhost';
-$usuario = 'root';
-$senha = '';
-$banco = 'valemobi';
+$servidor = 'fdb15.awardspace.net';
+$usuario = '2437263_valemobi';
+$senha = 'valemobi1';
+$banco = '2437263_valemobi';
 
 $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
 
